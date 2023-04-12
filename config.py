14e2290408a199bef4540ba57f9e8de3a0e8a87b
Belugastik <129519928+Belugastik@users.noreply.github.com>
2023-04-12 17:07:33 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
 from enum import Enum
 
-token = "6091645064:AAFzQGkcf3wMpZpir3EApkXEnEIGRc22npg"
+token = "5690982401:AAErG4DmAkdnHBvJs2nkactdLhJL0opHpdA"
 
 class States(Enum):
     S_START = "0"  #
     S_ENTER_NAME = "1"
     S_ENTER_CITY = "2"
     S_SEND_WEATHER_DAY = "3"
+    S_SEND_MESSAGE_GOOD_BEY = "4"
+
